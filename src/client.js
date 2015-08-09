5fb7b7fc938382dfba6307131d06580927db081e
guumaster <gustavoandresmarin@gmail.com>
2015-08-09 18:01:15 +0200

    changed token usage. added authorizations methods.

--- src/client.js
+++ src/client.js
@@ -6,7 +6,17 @@ var config = require('./config');
 
 var proto;
 
-var FitbitClient = function(clientId, consumerSecret) {
+var FitbitClient = function(clientId, consumerSecret, options) {
+
+  options = options || {};
+
+  if (!clientId) {
+    throw new Error('Missing clientId parameter');
+  }
+
+  if (!consumerSecret) {
+    throw new Error('Missing consumerSecret parameter');
+  }
 
   // Set the client credentials and the OAuth2 server
   this.oauth2 = oauth2({
@@ -18,63 +28,92 @@ var FitbitClient = function(clientId, consumerSecret) {
     useBasicAuthorizationHeader: true
   });
 
+  this.redirect_uri = options.redirect_uri;
+  this.scope = options.scope || config.FITBIT_DEFAULT_SCOPE;
+
 };
 
 proto = FitbitClient.prototype;
 
-proto.setTokens = function(accessToken, refreshToken, expiresIn) {
-  this.token = this.oauth2.accessToken.create({
-    access_token: accessToken,
-    refresh_token: refreshToken,
-    expires_in: expiresIn
-  });
-};
+proto.createToken = function(tokenObj) {
 
-proto.refreshAccessToken = function(options) {
-  var _this = this;
-  options = options || {};
-
-  if( !this.token.expired() && !options.forceRefresh) {
-    return Promise.resolve(this.token);
+  if (tokenObj && tokenObj.create) {
+    return tokenObj;
   }
 
+  return this.oauth2.accessToken.create(tokenObj);
+};
+
+proto.getAuthorizationUrl = function(redirect_uri, scope, state) {
+
+  var options = {
+    redirect_uri: redirect_uri || this.redirect_uri,
+    scope: scope || this.scope
+  };
+
+  if (state) {
+    options.state = state;
+  }
+
+  return this.oauth2.authCode.authorizeURL(options);
+};
+
+proto.getToken = function(code, redirect_uri) {
+
+  var authCode = this.oauth2.authCode;
   return new Promise(function(resolve, reject) {
 
-    _this.token.refresh(function(err, token) {
-
-      if (err) {
-        return reject(err);
-      }
-
-      _this.token = token;
-      return resolve(token);
+    authCode.getToken({
+      code: code,
+      redirect_uri: redirect_uri
+    }, function(err, token) {
+      return err ? reject(err) : resolve(token);
     });
 
   });
 };
 
+proto.refreshAccessToken = function(token, options) {
+  options = options || {};
+  token = this.createToken(token);
 
-proto.getDailyActivitySummary = function(options) {
+  if( !token.expired() && !options.forceRefresh) {
+    return Promise.resolve(token);
+  }
+
+  return new Promise(function(resolve, reject) {
+
+    token.refresh(function(err, token) {
+      return err ? reject(err) : resolve(token);
+    });
+
+  });
+};
+
+proto.getDailyActivitySummary = function(token, options) {
   options = helper.buildDailyActivitySummaryOptions(options);
+  token = this.createToken(token);
 
   //TODO: improve this way of getting the token
-  options.access_token = this.token.token.access_token;
+  options.access_token = token.access_token;
   return helper.createRequestPromise(options);
 
 };
-proto.getTimeSeries = function(options) {
+proto.getTimeSeries = function(token, options) {
   options = helper.buildTimeSeriesOptions(options);
+  token = this.createToken(token);
 
   //TODO: improve this way of getting the token
-  options.access_token = this.token.token.access_token;
+  options.access_token = token.access_token;
   return helper.createRequestPromise(options);
 
 };
 
-proto.getIntradayTimeSeries = function(options) {
+proto.getIntradayTimeSeries = function(token, options) {
   options = helper.buildIntradayTimeSeriesOptions(options);
+  token = this.createToken(token);
 
-  options.access_token = this.token.token.access_token;
+  options.access_token = token.access_token;
   return helper.createRequestPromise(options);
 };
 
