e9f9c7bd37560f89e10e08e93fa316272a16cf19
guumaster <gustavoandresmarin@gmail.com>
2015-08-09 18:08:31 +0200

    splitted auth and API functionalities

--- src/client.js
+++ src/client.js
@@ -1,10 +1,8 @@
 var oauth2 = require('simple-oauth2');
-var Promise = require('promise');
 
-var helper = require('./helpers');
 var config = require('./config');
-
-var proto;
+var addAuth = require('./auth');
+var addAPI = require('./api');
 
 var FitbitClient = function(clientId, consumerSecret, options) {
 
@@ -33,88 +31,7 @@ var FitbitClient = function(clientId, consumerSecret, options) {
 
 };
 
-proto = FitbitClient.prototype;
-
-proto.createToken = function(tokenObj) {
-
-  if (tokenObj && tokenObj.create) {
-    return tokenObj;
-  }
-
-  return this.oauth2.accessToken.create(tokenObj);
-};
-
-proto.getAuthorizationUrl = function(redirect_uri, scope, state) {
-
-  var options = {
-    redirect_uri: redirect_uri || this.redirect_uri,
-    scope: scope || this.scope
-  };
-
-  if (state) {
-    options.state = state;
-  }
-
-  return this.oauth2.authCode.authorizeURL(options);
-};
-
-proto.getToken = function(code, redirect_uri) {
-
-  var authCode = this.oauth2.authCode;
-  return new Promise(function(resolve, reject) {
-
-    authCode.getToken({
-      code: code,
-      redirect_uri: redirect_uri
-    }, function(err, token) {
-      return err ? reject(err) : resolve(token);
-    });
-
-  });
-};
-
-proto.refreshAccessToken = function(token, options) {
-  options = options || {};
-  token = this.createToken(token);
-
-  if( !token.expired() && !options.forceRefresh) {
-    return Promise.resolve(token);
-  }
-
-  return new Promise(function(resolve, reject) {
-
-    token.refresh(function(err, token) {
-      return err ? reject(err) : resolve(token);
-    });
-
-  });
-};
-
-proto.getDailyActivitySummary = function(token, options) {
-  options = helper.buildDailyActivitySummaryOptions(options);
-  token = this.createToken(token);
-
-  //TODO: improve this way of getting the token
-  options.access_token = token.access_token;
-  return helper.createRequestPromise(options);
-
-};
-proto.getTimeSeries = function(token, options) {
-  options = helper.buildTimeSeriesOptions(options);
-  token = this.createToken(token);
-
-  //TODO: improve this way of getting the token
-  options.access_token = token.access_token;
-  return helper.createRequestPromise(options);
-
-};
-
-proto.getIntradayTimeSeries = function(token, options) {
-  options = helper.buildIntradayTimeSeriesOptions(options);
-  token = this.createToken(token);
-
-  options.access_token = token.access_token;
-  return helper.createRequestPromise(options);
-};
+addAuth(FitbitClient.prototype);
+addAPI(FitbitClient.prototype);
 
 module.exports = FitbitClient;
