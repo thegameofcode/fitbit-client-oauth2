b6fd0c9ad7897066bfd6abe532a878cfbd03bfbc
Stephen Bluck <steve@stephens-imac.home>
2016-01-15 00:53:17 +0000

    fixed an issue where the specified scopes were not returned

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const FITBIT_BASE_API_URL = 'https://api.fitbit.com';
 const FITBIT_AUTH_PATH = '/oauth2/authorize';
 const FITBIT_TOKEN_PATH = '/oauth2/token';
-const FITBIT_DEFAULT_SCOPE = [ 'activity', 'nutrition', 'profile', 'settings', 'sleep', 'social', 'weight' ];
+const FITBIT_DEFAULT_SCOPE = 'activity nutrition profile settings sleep social weight';
 
 module.exports = {
   FITBIT_BASE_API_URL: FITBIT_BASE_API_URL,
